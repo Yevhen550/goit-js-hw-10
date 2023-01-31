cf0001fa2e43a87ce48a70d87767a67f113979d1
Yevhen Liaskivskyi <benatanlev@icloud.com>
2023-01-31 18:57:53 +0200

    git

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+export const fetchCountries = name => {
+  const LINK = 'https://restcountries.com/v2';
+  const FILTER_RESPONSE = 'name,capital,population,flags,languages';
+
+  return fetch(`${LINK}/name/${name}?fields=${FILTER_RESPONSE}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+};
